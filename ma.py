c6813385181c724e6534dc9067901799f81a3f03
Piromu <ac194601@edu.okinawa-ct.ac.jp>
2020-04-08 16:30:25 +0900

    UNITY ma.py

--- ma.py
+++ ma.py
@@ -6,11 +6,11 @@ def moving_average(arr, l):
 	if lenarr % 2 == 1 and l % 2 == 1:
 		return conv(arr, l)
 	elif lenarr % 2 == 0 and l % 2 == 1:
-		return np.append(conv(arr[:lenarr-1], l), arr[-1])
+		return np.append(conv(arr[:lenarr - 1], l), arr[-1])
 	elif lenarr % 2 == 1 and l % 2 == 0:
 		return conv(arr, l-1)
 	else:
-		return np.append(conv(arr[:lenarr-1], l-1), arr[-1])
+		return np.append(conv(arr[:lenarr - 1], l - 1), arr[-1])
 
 def conv_left(arr):
 	lenarr = len(arr)
@@ -19,7 +19,7 @@ def conv_left(arr):
 		return arr
 	else:
 		left = conv_left(arr[:lenarr - 2])
-		center = np.convolve(arr, np.ones(lenarr)/float(lenarr), mode='valid')
+		center = np.convolve(arr, np.ones(lenarr) / float(lenarr), mode='valid')
 		return np.hstack((left, center))
 
 def conv_right(arr):
@@ -28,7 +28,7 @@ def conv_right(arr):
 	if lenarr == 1:
 		return arr
 	else:
-		center = np.convolve(arr, np.ones(lenarr)/float(lenarr), mode='valid')
+		center = np.convolve(arr, np.ones(lenarr) / float(lenarr), mode='valid')
 		right = conv_right(arr[2:])
 		return np.hstack((center, right))
 
@@ -40,13 +40,12 @@ def conv(arr, l):
 	elif lenarr < l:
 		return conv(arr, l - 2)
 	else :
-		left = conv_left(arr[:l-2])
-		center = np.convolve(arr, np.ones(l)/float(l), mode='valid')
-		right = conv_right(arr[lenarr-l+2:])
+		left = conv_left(arr[:l - 2])
+		center = np.convolve(arr, np.ones(l) / float(l), mode='valid')
+		right = conv_right(arr[lenarr - l + 2:])
 
 		return np.hstack((left, center, right))
 
 if __name__ == "__main__":
 	arr = np.array([i for i in range(31)])
 	print(moving_average(arr, 9))
-	print()
